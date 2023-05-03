fd107fd5d151dbc0c177b9c0e7dd767036667254
Kan Rinata <rkanrinata@gmail.com>
2023-05-03 17:17:19 +0600

    lesson_05 files

--- firstsite/urls.py
+++ firstsite/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('bboard/', include('bboard.urls')),
+    path('', include('bboard.urls')),
 ]
