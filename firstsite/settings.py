3dd5690de6d3e98b9fd95b505710e18b39ee34e4
Kan Rinata <rkanrinata@gmail.com>
2023-05-15 15:36:54 +0600

    ABSOLUTE_URL_OVERRIDES

--- firstsite/settings.py
+++ firstsite/settings.py
@@ -53,6 +53,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'firstsite.urls'
 
+ABSOLUTE_URL_OVERRIDES = {
+    "bboard.rubric": lambda rec: f"/{rec.pk}/",
+
+}
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
